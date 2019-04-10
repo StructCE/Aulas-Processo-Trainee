6eccd4d94ec017f579cd26d9beaf3823d6605db6
Otho T.Komatsu <33355906+Shothogun@users.noreply.github.com>
2019-04-10 14:47:21 -0300

    Arquivos apresentados na aula

---
+++ aula_Ruby/pato.rb
@@ -0,0 +1,13 @@
+class Pato
+	def initialize (nome)
+		@nome = nome
+	end
+
+	def nome
+		@nome
+	end
+	
+	def quack
+		puts "QUACK!"
+	end
+end 
---
+++ aula_Ruby/test.rb
@@ -0,0 +1,7 @@
+require './pato'
+
+p1 = Pato.new('Lisandro')
+
+puts "Olá meu nome é #{p1.nome}"
+
+p1.quack
